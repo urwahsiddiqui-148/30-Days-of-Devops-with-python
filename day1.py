480986b56f218a6bd28332dd0626c88e95e8fa8c
urwahsiddiqui-148 <urwahshafiq23@gmail.com>
2025-08-31 00:36:20 +0500

    first repo commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+print("i am urwah shafiq siddiqui")
+a = 10
+b = 20
+d = 49
+c = a*b+d
+print (c)
+print("the value of the c is", c)
+list = [1,2,3,4,5,6,7,8,90,100]
+list2 = [23,5,54,345,45,345,356,335,6,3,3,4,55,6,33]
+sum = list + list2
+print ("the sume of the both list are", sum)
+print (sum)
+#these are the some of the pracices , some basi functions and some of the basic syntax of python
+
